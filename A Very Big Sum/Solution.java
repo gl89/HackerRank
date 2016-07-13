0f90d5b7ab60dd349eefb6934f4c87726ad693f2
gl89 <gloterena@gmail.com>
2016-07-12 21:37:08 -0400

    My first commit

---
+++ A Very Big Sum/Solution.java
@@ -0,0 +1,30 @@
+/*
+Gabriel Loterena
+7/12/2016
+Hackerrank 
+Numbers bigger than 2^32 need long to work. I typecasted it to long.
+Similar problem in Codility. 
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        long sum = 0;
+        for(int arr_i=0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+        }
+        for(int j=0; j < n; j++){
+            sum+= (long)arr[j];
+        }
+        System.out.println(sum);
+    }
+}
