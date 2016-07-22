4dc308b6a1702ee4108c3c8e338a89a0a54eb2dc
gl89 <gloterena@gmail.com>
2016-07-22 00:23:48 -0400

    Refactored Balance.java

--- Balancing/Balance.java
+++ Balancing/Balance.java
@@ -7,24 +7,35 @@ You learn from mistakes
 import java.util.*;
 
 public class Balance{
-
-   public static void main(String[] args){
-   boolean balanced=true;
-   String str="()[]{}"; 
    
-   Map<Character,Character> m = new HashMap<Character,Character>();
-   Stack<Character> stack = new Stack<Character>();
    
-   m.put('{','}');
-   m.put('(',')');
-   m.put('[',']');
+   public static boolean isBalanced(String str){
+      Map<Character,Character> m = new HashMap<Character,Character>();
+      Stack<Character> stack = new Stack<Character>();
    
-   for(int i=0;i<str.length();i++){
-      if(m.containsKey(str.charAt(i)))
-         stack.push(str.charAt(i));
-      else if(stack.empty() || str.charAt(i)==stack.pop());
-         balanced=false;
+      //Nice way to match K,V
+      m.put('{','}');
+      m.put('(',')');
+      m.put('[',']');
+      if(str.length()==0)
+         return false;
+      if(str.length()%2!=0)
+         return false;
+      
+      for(int i=0;i<str.length();i++){
+         if(m.containsKey(str.charAt(i))){
+            stack.push(str.charAt(i));
+         }else if(stack.empty()||(str.charAt(i)!= m.get(stack.pop()))){ //the key acts a match for the vaule
+            return false;
+         }
+      }
+      return true;
    }
-   System.out.print(balanced);
+   
+   public static void main(String[] args){ 
+      System.out.println(isBalanced("[]"));
+      System.out.println(isBalanced("[][]{}{}()()"));
+      System.out.println(isBalanced("()()()()()())"));
+      System.out.println(isBalanced(""));
    }
 }
