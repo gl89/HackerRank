8f5e750370fc1fd1d1f620a9c53e9876f30dc377
gl89 <gloterena@gmail.com>
2016-08-07 00:29:21 -0400

    Added new challenged

---
+++ Day 22/Solution.java
@@ -0,0 +1,16 @@
+/*
+Gabriel Loterena
+8/3/2016
+Day 22 Find the height of the tree
+*/	
+public class Solution{
+   public static int getHeight(Node root){
+      if(root==null)
+         return 0;
+      if(root.left!=null)
+         return 1+Math.max(getHeight(root.left),getHeight(root.right));
+      if(root.right!=null)
+         return 1+Math.max(getHeight(root.left),getHeight(root.right));
+      return 0;
+    }
+ }
