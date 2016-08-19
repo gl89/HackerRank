cd68172a5432031bb54a442f50e70b706c800940
gl89 <gloterena@gmail.com>
2016-08-18 21:27:11 -0400

    Changed a a few lines

--- Pangrams/Solution.java
+++ Pangrams/Solution.java
@@ -20,34 +20,10 @@ public class Solution {
         line = line.toLowerCase();
         
         Map<Character,Boolean> alpha = new HashMap<Character,Boolean>();
-        
-        alpha.put('a',false);
-        alpha.put('b',false);
-        alpha.put('c',false);
-        alpha.put('d',false);
-        alpha.put('e',false);
-        alpha.put('f',false);
-        alpha.put('g',false);
-        alpha.put('h',false);
-        alpha.put('i',false);
-        alpha.put('j',false);
-        alpha.put('k',false);
-        alpha.put('l',false);
-        alpha.put('m',false);
-        alpha.put('n',false);
-        alpha.put('o',false);
-        alpha.put('p',false);
-        alpha.put('q',false);
-        alpha.put('r',false);
-        alpha.put('s',false);
-        alpha.put('t',false);
-        alpha.put('u',false);
-        alpha.put('v',false);
-        alpha.put('w',false);
-        alpha.put('x',false);
-        alpha.put('y',false);
-        alpha.put('z',false);
-        
+        for(char i = 'a';ai<='z';i++){
+            alpha.put(i,false);
+        }
+                   
         for(int i=0;i<line.length();i++){
             if(alpha.containsKey(line.charAt(i)))
                 alpha.put(line.charAt(i),true);
