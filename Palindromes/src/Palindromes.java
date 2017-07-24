ea41f0ff9fa747f60d47ddc1f3938935c8fe3830
gl89 <gloterena@gmail.com>
2017-07-24 01:29:11 -0400

    Relearning

---
+++ Palindromes/src/Palindromes.java
@@ -0,0 +1,23 @@
+//Probably not the best method to find palindromes, but the idea is
+//we will be going through the string and find all instances of substring
+//palindroms by using Stringbuilder to create a sub string character by character 
+//and then reversing. If they're equal they should be palindromes.
+
+public class Palindromes {
+	static void PalindromesFinder(String s){
+		int l = s.length();
+		
+
+		for(int i=0;i<l;i++){
+			
+			
+			
+		}
+		
+		
+	}
+	public static void main(String[] args){
+		
+		
+	}
+}
